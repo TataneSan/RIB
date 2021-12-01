fa1404161aa35356d245d207e796ccbf5f61a514
TataneSan <65590382+TataneSan@users.noreply.github.com>
2021-12-01 19:41:44 +0100

    Site

---
+++ pdfrib.js
@@ -0,0 +1,49 @@
+async function GenerateDoc() {
+  const formUrl = 'pdf.pdf'
+  const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+
+
+  const pdfDoc = await PDFLib.PDFDocument.load(formPdfBytes)
+
+  const form = pdfDoc.getForm()
+
+  const nom_prenom = form.getTextField('nom_prenom')
+  const addresse_rue = form.getTextField('adresse_rue')
+  const addresse_cpville = form.getTextField('adresse_cp-ville')
+  const iban_codeeta = form.getTextField('iban_code-eta')
+  const iban_codeguichet = form.getTextField('iban_code-guichet')
+  const iban_accnumber = form.getTextField('iban_acc-number')
+  const iban_clerib = form.getTextField('iban_clerib')
+  const iban_full = form.getTextField('iban_full')
+  const date = form.getTextField('date_of_the_day')
+
+  nom_prenom.setText(document.getElementById("name").value)
+  addresse_rue.setText(document.getElementById("addressStreet").value)
+  addresse_cpville.setText(document.getElementById("addressZipcode").value + " " + document.getElementById("addressCity").value)
+  
+  iban = document.getElementById("iban").value
+  ibanETA = iban.substr(4,iban.length-22)
+  ibanGUICHET = iban.substr(9, iban.length-22)
+  ibanACC = iban.substr(14, iban.length-16)
+  ibanKEY = iban.substr(25, iban.length)
+
+
+
+  iban_codeeta.setText(ibanETA)
+  iban_codeguichet.setText(ibanGUICHET)
+  iban_accnumber.setText(ibanACC)
+  iban_clerib.setText(ibanKEY)
+  iban_full.setText(iban)
+  
+
+  var d = new Date();
+  daydate = d.getDate().toString() + "/" + d.getMonth().toString() + "/" + d.getFullYear().toString()
+  console.log(daydate)
+
+  date.setText(daydate)
+
+  const pdfBytes = await pdfDoc.save()
+
+  download(pdfBytes, "ZelfRIB.pdf", "application/pdf");
+
+}
---
+++ saythanks.js
@@ -0,0 +1,17 @@
+function saythanks() {
+	confirmation = prompt("Indiquez votre prénom/pseudo pour accompagner le remerciement. \nMettez \"Anonyme\" si vous souhaitez le rester.");
+	
+
+	var request = new XMLHttpRequest();
+	request.open("POST", "https://discord.com/api/webhooks/915672744492949555/OtRir9GdarYLMgMQn59btW7-Ardfcnx-KpzmVDkR2Je89DKOSdMJ3zrOomt9D8MXcK2z");
+
+	request.setRequestHeader('Content-type', 'application/json');
+
+	var params = {
+    	username: "ZELF - RIB Generator page",
+    	avatar_url: "https://raw.githubusercontent.com/Poloin34/ZelfGenerateRib/master/favicon.png",
+    	content: "**" + confirmation.toString() + "** te remercie !"
+  	}
+
+  	request.send(JSON.stringify(params));
+}      
